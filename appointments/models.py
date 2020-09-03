b4c3454c8c51df7727dfea6257392f14d439ff04
Cleyson Lima <cleysonph@gmail.com>
2020-09-02 23:50:49 -0300

    Create the model Appointment

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Appointment(models.Model):
+    pet = models.ForeignKey(
+        "pets.Pet",
+        verbose_name="Pet",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+    )
+    date = models.DateField("Data da Consulta", auto_now=True, null=False, blank=False)
+    reason = models.CharField(
+        "Motivo da Consulta", max_length=200, null=False, blank=False
+    )
+    current_weight = models.FloatField("Peso Atual", null=False, blank=False)
+    current_drugs = models.TextField("Medicamento Atual", null=False, blank=True)
+    prescribed_drugs = models.TextField(
+        "Medicamentos Prescritos", null=False, blank=False
+    )
+    prescribed_exams = models.TextField("Exames Prescritos", null=False, blank=False)
+
+    class Meta:
+        verbose_name = "consulta"
+        verbose_name_plural = "consultas"
+
+    def __str__(self):
+        return f"{self.pet.name} - {self.date}"
---
+++ appointments/tests/test_models.py
@@ -0,0 +1,58 @@
+from django.test import TestCase
+from model_mommy import mommy
+
+from appointments.models import Appointment
+
+
+class AppointmentModelTest(TestCase):
+    def test_pet_label(self):
+        pet_label = Appointment._meta.get_field("pet").verbose_name
+        self.assertEqual(pet_label, "Pet")
+
+    def test_date_label(self):
+        date_label = Appointment._meta.get_field("date").verbose_name
+        self.assertEqual(date_label, "Data da Consulta")
+
+    def test_reason_label(self):
+        reason_label = Appointment._meta.get_field("reason").verbose_name
+        self.assertEqual(reason_label, "Motivo da Consulta")
+
+    def test_reason_max_length(self):
+        max_length = Appointment._meta.get_field("reason").max_length
+        self.assertEqual(max_length, 200)
+
+    def test_current_weight_label(self):
+        current_weight_label = Appointment._meta.get_field(
+            "current_weight"
+        ).verbose_name
+        self.assertEqual(current_weight_label, "Peso Atual")
+
+    def test_current_drugs_label(self):
+        current_drugs_label = Appointment._meta.get_field("current_drugs").verbose_name
+        self.assertEqual(current_drugs_label, "Medicamento Atual")
+
+    def test_prescribed_drugs_label(self):
+        prescribed_drugs_label = Appointment._meta.get_field(
+            "prescribed_drugs"
+        ).verbose_name
+        self.assertEqual(prescribed_drugs_label, "Medicamentos Prescritos")
+
+    def test_prescribed_exams_label(self):
+        prescribed_exams_label = Appointment._meta.get_field(
+            "prescribed_exams"
+        ).verbose_name
+        self.assertEqual(prescribed_exams_label, "Exames Prescritos")
+
+    def test_verbose_name(self):
+        verbose_name = Appointment._meta.verbose_name
+        self.assertEqual(verbose_name, "consulta")
+
+    def test_verbose_name_plural(self):
+        verbose_name_plural = Appointment._meta.verbose_name_plural
+        self.assertEqual(verbose_name_plural, "consultas")
+
+    def test_string_representation(self):
+        appointment = mommy.make(Appointment)
+        self.assertEqual(
+            str(appointment), f"{appointment.pet.name} - {appointment.date}"
+        )
