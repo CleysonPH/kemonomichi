d3921965a58de1a0e9d74ccc30d0d6f2ded66a86
Cleyson Lima <cleysonph@gmail.com>
2020-09-02 21:00:23 -0300

    Create the model Pet

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Pet(models.Model):
+    SPECIE_CHOICES = (
+        ("CA", "Cachorro"),
+        ("GA", "Gato"),
+        ("CO", "Coelho"),
+    )
+
+    COLOR_CHOICES = (
+        ("PR", "Preto"),
+        ("BR", "Branco"),
+        ("CI", "Cinza"),
+        ("MA", "Marrom"),
+    )
+
+    name = models.CharField("Nome", max_length=50, null=False, blank=False)
+    birth_date = models.DateField("Data de Nascimento", null=False, blank=False)
+    specie = models.CharField(
+        "Espécie", max_length=2, choices=SPECIE_CHOICES, null=False, blank=False
+    )
+    color = models.CharField(
+        "Cor", max_length=2, choices=COLOR_CHOICES, null=False, blank=False
+    )
+    owner = models.ForeignKey(
+        "clients.Client",
+        verbose_name="Dono",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+    )
+
+    class Meta:
+        verbose_name = "pet"
+        verbose_name_plural = "pets"
+
+    def __str__(self):
+        return self.name
---
+++ pets/tests/test_models.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from model_mommy import mommy
+
+from pets.models import Pet
+
+
+class PetModelTest(TestCase):
+    def test_name_label(self):
+        name_label = Pet._meta.get_field("name").verbose_name
+        self.assertEqual(name_label, "Nome")
+
+    def test_name_max_length(self):
+        max_length = Pet._meta.get_field("name").max_length
+        self.assertEqual(max_length, 50)
+
+    def test_birth_date_label(self):
+        birth_date_label = Pet._meta.get_field("birth_date").verbose_name
+        self.assertEqual(birth_date_label, "Data de Nascimento")
+
+    def test_specie_label(self):
+        specie_label = Pet._meta.get_field("specie").verbose_name
+        self.assertEqual(specie_label, "Espécie")
+
+    def test_specie_max_length(self):
+        max_length = Pet._meta.get_field("specie").max_length
+        self.assertEqual(max_length, 2)
+
+    def test_color_label(self):
+        color_label = Pet._meta.get_field("color").verbose_name
+        self.assertEqual(color_label, "Cor")
+
+    def test_color_max_length(self):
+        max_length = Pet._meta.get_field("color").max_length
+        self.assertEqual(max_length, 2)
+
+    def test_owner_label(self):
+        owner_label = Pet._meta.get_field("owner").verbose_name
+        self.assertEqual(owner_label, "Dono")
+
+    def test_string_representation(self):
+        pet = mommy.make(Pet)
+        self.assertEquals(str(pet), pet.name)
+
+    def test_verbose_name(self):
+        verbose_name = Pet._meta.verbose_name
+        self.assertEquals(str(verbose_name), "pet")
+
+    def test_verbose_name_plural(self):
+        verbose_name_plural = Pet._meta.verbose_name_plural
+        self.assertEquals(str(verbose_name_plural), "pets")
