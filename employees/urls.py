8b6af6056ff8c93ddca8dd3c64b33d7ee8cc482e
Cleyson Lima <cleysonph@gmail.com>
2020-09-11 16:51:47 -0300

    Create a profile page

--- employees/urls.py
+++ employees/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import employee_list
+from .views import employee_list, profile
 
 
 app_name = "employees"
 urlpatterns = [
     path("", employee_list, name="employee-list"),
+    path("perfil/", profile, name="profile"),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.contrib.admin.models import LogEntry
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 from .models import Employee
 
@@ -14,3 +15,17 @@ def employee_list(request):
     }
 
     return render(request, "employees/employee_list.html", context)
+
+
+@login_required
+def profile(request):
+    employee = request.user
+    logs = LogEntry.objects.filter(user=employee.pk)
+
+    context = {
+        "title": f"Perfil de {employee.first_name}",
+        "employee": employee,
+        "logs": logs,
+    }
+
+    return render(request, "employees/employee_detail.html", context)
